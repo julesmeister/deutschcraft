4de26102b489d8c5104babc1d184460d0e3eaa56
Jules Lee <jackconsolidated@gmail.com>
2026-02-15 20:47:57 +0800

    Update statsCalculatorAdmin.ts

--- lib/services/writing/statsCalculatorAdmin.ts
+++ lib/services/writing/statsCalculatorAdmin.ts
@@ -1,15 +1,6 @@
 /**
- * Writing Stats Calculator (Admin) - Switcher
- * Automatically switches between Firebase and Turso implementations
+ * Writing Stats Calculator (Admin)
+ * Uses Turso implementation directly
  */
 
-import * as firebaseImpl from './firebase/statsCalculatorAdmin';
-import * as tursoImpl from '../turso/writing/statsCalculatorAdmin';
-
-const USE_TURSO = process.env.NEXT_PUBLIC_USE_TURSO === 'true';
-
-const implementation = USE_TURSO ? tursoImpl : firebaseImpl;
-
-export const {
-  recalculateWritingStatsAdmin,
-} = implementation;
+export { recalculateWritingStatsAdmin } from '../turso/writing/statsCalculatorAdmin';
